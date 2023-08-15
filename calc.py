4626f25e343da2e744a8c3d4df2acd775efc2e68
Dhammadip Bharane <122970746+DhammadipBharane@users.noreply.github.com>
2023-08-15 15:44:00 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,17 @@
+'''This is Python Calculator'''
+
+__name__ = "Calculator"
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def fldiv(a,b):
+    return a//b
+def pow(a,b):
+    return a**b
+
